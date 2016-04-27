decbb40a523a0b5fd62d0b26db644abfd03039b1
Jay Hirata <jay@idahoscientific.com>
2016-04-26 22:57:21 -0600

    add some documentation and pkt header and decode

--- dropship.go
+++ dropship.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	//    "io"
+	//"bytes"
+	"encoding/binary"
+	//"errors"
 	"github.com/klauspost/reedsolomon"
 	"math/rand"
 	"net"
@@ -30,6 +33,51 @@ func (s *Session) String() string {
 	)
 }
 
+// PktHeader generates a string representing the packet header to be sent with data.
+func (s *Session) PktHeader() []byte {
+
+	buf := make([]byte, 50) // arbitrary value large enough to hold all encoded bits
+	sum := 0
+
+	sum += binary.PutUvarint(buf, uint64(s.Id))
+	sum += binary.PutUvarint(buf[sum:], uint64(s.Sequence))
+	sum += binary.PutUvarint(buf[sum:], uint64(s.K))
+	sum += binary.PutUvarint(buf[sum:], uint64(s.M))
+	sum += binary.PutUvarint(buf[sum:], uint64(s.PktSize))
+
+	return buf[:sum]
+}
+
+func DecodePktHeader(data []byte) (*Session, error) {
+	var s Session
+
+	offset := 0
+	v, n := binary.Uvarint(data)
+	s.Id = int(v)
+	offset += n
+
+	v, n = binary.Uvarint(data[offset:])
+	s.Sequence = int(v)
+	offset += n
+
+	v, n = binary.Uvarint(data[offset:])
+	s.K = int(v)
+	offset += n
+
+	v, n = binary.Uvarint(data[offset:])
+	s.M = int(v)
+	offset += n
+
+	v, n = binary.Uvarint(data[offset:])
+	s.PktSize = int(v)
+	offset += n
+
+	return &s, nil
+}
+
+// Send sends data with reed solomon encoded parity data to the destination
+// specified in the Session. It returns the amount of data sent and nil error
+// on success.
 func (s *Session) Send(data []byte) (int, error) {
 	size := len(data)
 	burst_size := (s.K + s.M) * s.PktSize
@@ -60,17 +108,13 @@ func (s *Session) Send(data []byte) (int, error) {
 		return 0, err
 	}
 
-	str := fmt.Sprintf("%#x,%d,%d,%d,", s.Id, s.K, s.M, s.PktSize)
-	var header []byte
-	header = append(header, []byte(str)...)
+	header := s.PktHeader()
 
 	for _, d := range parts {
-		seq := fmt.Sprintf("%#016x.", s.Sequence)
-		tmp := append(header, seq...)
-		tmp = append(tmp, d...)
+		tmp := append(header, d...)
 		n, err := s.Connection.Write(tmp)
 		if n != len(tmp) {
-			fmt.Printf("Couldn't send whole packet: %d of %d\n", n, s.PktSize+len(str))
+			fmt.Printf("Couldn't send whole packet: %d of %d\n", n, s.PktSize+len(header))
 			return 0, err
 		}
 		if err != nil {
@@ -83,6 +127,13 @@ func (s *Session) Send(data []byte) (int, error) {
 	return size, nil
 }
 
+// NewSession creates the information required for setting up a new DropShip
+// session. The parameters k and m are used to specify the number of data parts
+// and parity parts respectively. The parameter size determines how big each
+// data and parity part should be. Dest is a string representation of the
+// remote IP and port. See the "net" package for more details on the dest
+// parameter. On success, NewSession will return a Session and nil error. On
+// error, Session will be nil and the error will be set.
 func NewSession(k, m, size int, dest string) (*Session, error) {
 	addr, err := net.ResolveUDPAddr("udp", dest)
 	if err != nil {
@@ -120,6 +171,7 @@ func main() {
 		return
 	}
 
+	fmt.Printf("Header: % x", s.PktHeader)
 	msg := "Hello world! We're going to send a small message to test this out"
 
 	n, err := s.Send([]byte(msg))
